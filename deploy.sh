9a7fe014d5bf67efdbbfa9e5edb0fdff845bf63e
Subrata Mazumder <subrata.besu@gmail.com>
2019-10-20 16:09:28 +0530

    read me updated

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-aws s3 sync ./ s3://scpauljewellers.com/ --acl public-read --exclude "*.git*" --exclude "*.DS*" --exclude "*deploy.sh*"  --profile subrata-amazon
+aws s3 sync ./ s3://scpauljewellers.com/ --acl public-read --exclude "*.git*" --exclude "*.DS*" --exclude "*.sh*"  --profile subrata-amazon
